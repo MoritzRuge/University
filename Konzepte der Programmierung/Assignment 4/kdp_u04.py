ba7b9a10959381fa50efa53674ec3436ae188405
Moritz Ruge <ruge.moritz@gmail.com>
2024-11-14 00:32:36 +0100

    tried to implement subtask b)

--- Konzepte der Programmierung/Assignment 4/kdp_u04.py
+++ Konzepte der Programmierung/Assignment 4/kdp_u04.py
@@ -16,24 +16,48 @@ def experiment(n):
     while len(drawn_numbers) < n:
         number = random.randint(1, n)
         drawn_numbers.add(number)
+        number_key, number_value = count_number(number)
         counter +=1
-    return counter
+    return counter, number_key, number_value
 
 def run_experiment(scope, limit=100):
-    result = [experiment(scope) for _ in range(limit)]
+    result, number_key, number_value = [experiment(scope) for _ in range(limit)]
     min_result = min(result)
     max_result = max(result)
     average_result = np.mean(result)
-    return min_result, max_result, average_result
+    return min_result, max_result, average_result, number_key, number_value
 
 
-for n in [10, 100, 1000]:
-    min_result, max_result, average_result = run_experiment(n)
-    predicted_result = n * sum(1/k for k in range(1, n+1))
-    print(f"n={n}")
-    print(f"Minimum: {min_result}")
-    print(f"Maximum: {max_result}")
-    print(f"Average: {average_result:.0f}")
-    print(f"Predicted: {predicted_result:.0f}")
+def count_number(number):
+    number_dictionary = dict()
+    
+    number_dictionary[number] = 
+    # search for the number and raising the value by one
+    number_dictionary[number] += 1
+    
+    number_value = 0
+    # loop through the dic to get the highest value
+    for _ in number_dictionary:
+        if number_dictionary[number] > number_value:
+            number_value = number_dictionary[number].get()
+            number_key = number_dictionary[number]
 
 
+    # return the highest number and the count
+    return number_key, number_value
+
+
+def main():
+    for n in [10, 100, 1000]:
+        min_result, max_result, average_result, number_key, number_value = run_experiment(n)
+        predicted_result = n * sum(1/k for k in range(1, n+1))
+        print(f"n={n}")
+        print(f"Minimum: {min_result}")
+        print(f"Maximum: {max_result}")
+        print(f"Average: {average_result:.0f}")
+        print(f"Predicted: {predicted_result:.0f}")
+        print(f"Highest number is {number_key} with {number_value}x pick")
+
+
+if __name__ == "__main__":
+    main()
