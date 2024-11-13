a2717f81a95efe407c22d702e23570d9e2a02baa
Moritz Ruge <ruge.moritz@gmail.com>
2024-11-13 23:35:30 +0100

    Finished Task 1

--- Konzepte der Programmierung/Assignment 4/kdp_u04.py
+++ Konzepte der Programmierung/Assignment 4/kdp_u04.py
@@ -8,17 +8,32 @@
 """
 
 import random
+import numpy as np
 
-# function which should count all random numbers and store them in a set of numbers
-def numberStore():
-    numberset = set(())
+def experiment(n):
+    drawn_numbers = set()
     counter = 0
-    for i in range(10):
-        n = random.randint(1, 10)
-        numberset.add(n)
-        counter += 1
+    while len(drawn_numbers) < n:
+        number = random.randint(1, n)
+        drawn_numbers.add(number)
+        counter +=1
+    return counter
 
-    print(numberset , " and Counter:" ,counter)
+def run_experiment(scope, limit=100):
+    result = [experiment(scope) for _ in range(limit)]
+    min_result = min(result)
+    max_result = max(result)
+    average_result = np.mean(result)
+    return min_result, max_result, average_result
 
 
-numberStore()
+for n in [10, 100, 1000]:
+    min_result, max_result, average_result = run_experiment(n)
+    predicted_result = n * sum(1/k for k in range(1, n+1))
+    print(f"n={n}")
+    print(f"Minimum: {min_result}")
+    print(f"Maximum: {max_result}")
+    print(f"Average: {average_result:.0f}")
+    print(f"Predicted: {predicted_result:.0f}")
+
+
