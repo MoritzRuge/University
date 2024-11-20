91df3a3f0db987706c1ff7336eba99cf887f48ba
Moritz Ruge <ruge.moritz@gmail.com>
2024-11-20 21:26:07 +0100

    First Commit on exercise sheet 5

---
+++ Konzepte der Programmierung/Assignment 5/u05.py
@@ -0,0 +1,11 @@
+"""
+ToDo:
+- Zeile soll aus 80 zeichen bestehen
+- Funktion soll eine Liste von Zeilen wiedergeben
+- Funktion soll die eingabe in Blocksatz formatieren
+"""
+
+
+def Blocksatz():
+    #duh code much
+    
