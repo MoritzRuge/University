c3e73c1aea0dcbcc854925d82274987b0f850cf1
Moritz Ruge <ruge.moritz@gmail.com>
2024-11-22 10:08:57 +0100

    added format_block funktion, which currently does not work

--- Konzepte der Programmierung/Assignment 5/u05.py
+++ Konzepte der Programmierung/Assignment 5/u05.py
@@ -10,8 +10,10 @@ def main():
     user_input = input("Bitte geben Sie ihren Text ein: ")
 
     #print the result
-    print(Blocksatz(user_input.split()))
+    result = Blocksatz(user_input.split())
 
+    for line in result:
+        print(f"|{line}|")
 
 def Blocksatz(input_list):
     #duh code much
@@ -31,7 +33,7 @@ def Blocksatz(input_list):
             word = word[:linelimit]
 
         # check if the word is able to fit on the line
-        if current_length and len(word) > linelimit:
+        if current_length and len(word) and len(current_line) > linelimit:
             # if the word is to long to fit, format the current line into blocksatz and create a new line
             lines.append(format_block(current_line, linelimit))
             current_length = 0
@@ -49,7 +51,23 @@ def Blocksatz(input_list):
 
     return lines
 
-def format_block(current_line, linelimit):
+def format_block(words, linelimit):
+    """Formating one line in Blocksatz"""
+    if len(words) == 1:
+        return words[0].ljust(linelimit)
+    
+    #calculatin the total length in the given list
+    total_length = sum(len(word) for word in words)
+    spaces_needed = linelimit - total_length
+    spaces_between_words, extra_spaces = divmod(spaces_needed, len(words)-1)
+
+    justified_line = ""
+    for i, word in enumerate(words[:-1]):
+        justified_line += word
+        justified_line += " " * (spaces_between_words + (1 if i < extra_spaces else 0))
+    justified_line += words[-1]
+
+    return justified_line
 
 
 
