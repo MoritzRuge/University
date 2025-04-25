8bc3a9d7a0b86aa86d730751949129a275977f97
Moritz Ruge <ruge.moritz@gmail.com>
2025-04-25 14:19:55 +0200

    Added the Sommer Semester Courses

---
+++ Algorithmen und Datenstrukturen/Tutorium/Lösung_1.md
@@ -0,0 +1,4 @@
+Aufgabe 1: Binäre Heaps und binäre Suchäume I
+	Nennen Sie die Eigenschaften eines binären Suchbaums und die Eigenschaften eines binären Heaps. Was sind Gemeinsamkeiten, was sind Unterschiede?
+
+Antwort:
---
+++ BKS/helloworld.c
@@ -0,0 +1,11 @@
+// tutorial unter https://git.imp.fu-berlin.de/bks-tuts/bks-exercises/-/wikis/C-Kurs/Hello-World
+//
+//
+// Header std für standard io für in output
+// bibliothek bietet z.b. auch printf
+#include <stdio.h>
+
+int main(){
+	printf("Hello World!");
+	return 0;
+}
