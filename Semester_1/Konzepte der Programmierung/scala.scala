dba2bb723cba020ee9fa97d0117a3be65cfeaf38
Moritz Ruge <ruge.moritz@gmail.com>
2025-06-06 12:43:54 +0200

    init push, implementierung alles Sortieralgorithmen in Scala

---
+++ Semester_1/Konzepte der Programmierung/scala.scala
@@ -0,0 +1,12 @@
+// Todo: Implimentiere die Suchalgorithmen mit festverbauter liste oder liste zum eingeben
+//
+
+// Import
+
+
+// Selection Sort
+// Suche das kleinste Element und setzte es an die erste Stelle usw.
+// Benötigt eine swap funktion zum tauschen der Elemente und die Normale Sortingfunktion
+//
+
+
